81cc283363e01b67086343376a3b7bbcc19d761b
Gijs-W <gijsvandewater+github@gmail.com>
2015-01-06 12:56:48 +0100

    Kerkhof

--- Citadels Server/WarlordState.cpp
+++ Citadels Server/WarlordState.cpp
@@ -52,8 +52,25 @@ void WarlordState::Handle(GameRunningState& context, GameManager& gm){
 		choosenVictim->GetCityCardContainer()->Push_Back(removedCard);
 	}
 	else {
-		gm.GetCardManager()->GetDistrictCardDiscardPile()->Push_Back(removedCard);
-		choosenVictim->Send("You building " + removedCard->GetName() + " has been destroyed by " + m_CurrentPlayer->GetName());
+
+		if (choosenVictim->GetCityCardContainer()->HasCard("Kerkhof") && choosenVictim->GetGoldPieces() >= 1) {
+			int action = choosenVictim->RequestInput("The building " + removedCard->GetName() + " is about to be destroyed. Would you like to keep it?", vector < string > { "yes", "no"});
+
+			if (action == 0) {
+				choosenVictim->GetDistrictCardContainer()->Push_Back(removedCard);
+				choosenVictim->Send("The card has been added to your stock pile");
+			}
+			else {
+				gm.GetCardManager()->GetDistrictCardDiscardPile()->Push_Back(removedCard);
+			}
+
+		}
+		else {
+			gm.GetCardManager()->GetDistrictCardDiscardPile()->Push_Back(removedCard);
+			choosenVictim->Send("You building " + removedCard->GetName() + " has been destroyed by " + m_CurrentPlayer->GetName());
+		}
+		
+		
 		m_CurrentPlayer->SetGoldPieces(m_CurrentPlayer->GetGoldPieces() - (removedCard->getCost() - 1));
 		m_CurrentPlayer->Send("Building succesfully destroyed!");
 	}
